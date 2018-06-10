2b90a6e4dad8877f58c137ec684f5808ca9e98a9
Vytautas Žilinas <vytautaszilinas@gmail.com>
2018-06-10 20:49:56 +0300

    testing adapter for sql done

--- data-generator-python/test-data-catcher-sql.py
+++ data-generator-python/test-data-catcher-sql.py
@@ -1,13 +1,32 @@
 from kafka import KafkaConsumer
+from os import getenv
+import json
+import pyodbc
 
-# To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer('generate-report',
-                         bootstrap_servers=['localhost:9092'],
-                         auto_offset_reset='earliest')
-KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
+
+consumer = KafkaConsumer('generate_report',
+                         bootstrap_servers=['localhost:9092'])
+                         #auto_offset_reset='earliest')
+
+KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+conn = pyodbc.connect(
+    r'DRIVER={SQL Server};'
+    r'SERVER=localhost\SQLEXPRESS;'
+    r'DATABASE=Kursinis;'
+    r'Trusted_Connection=yes;'
+    )
+
+cursor = conn.cursor()
+sqlinsert = """
+        INSERT INTO [dbo].[Buys]
+                ([Amount]
+                ,[ProductId])
+            VALUES
+                ({amount}
+                ,{product_id})
+    """
 for message in consumer:
-    # message value and key are raw bytes -- decode if necessary!
-    # e.g., for unicode: `message.value.decode('utf-8')`
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+    data = json.loads(message.value)
+    insertquery= sqlinsert.format(amount=data['amount'], product_id=data["product_id"])
+    cursor.execute(insertquery)
+    conn.commit()
--- data-generator-python/test-data-generator.py
+++ data-generator-python/test-data-generator.py
@@ -1,4 +1,5 @@
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
 import json
 import random
 import string
@@ -6,15 +7,17 @@ import time
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
+
 throttle_amount = 0.0005 #defines the amount of throttle required to reach optimal throughput
 for x in range(0, 100000):
+	time.sleep(throttle_amount)
 	data = {}
-	data['price'] = random.random() * 1000
+	data['price'] = round(random.random() * 100, 3)
 	data['product_id'] = random.randint(1, 2000000)
 	data['tax_percent'] = random.randint(1, 20)
 	data['amount'] = random.randint(1, 5)
 	data['shop_name'] = "".join([random.choice(string.letters[:5]) for i in xrange(5)])
 	json_data = json.dumps(data).encode('utf-8')
 	producer.send('generate_report',value=json_data)
-	time.sleep(throttle_amount)
 
+producer.flush()
